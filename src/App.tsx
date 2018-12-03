a0015936eb2d21bf0ad08145937ccbbd14d4b18c
NarekoGhazaryan <narekcods@gmail.com>
2018-12-03 12:13:21 +0400

    use context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import TodoApp from "./components/TodoApp";
 import UserContext from './contexts/UserContext'
+import UserUpdateContext from './contexts/UserUpdateContext'
 
 class App extends Component<any, any> {
   constructor(props) {
@@ -24,11 +25,24 @@ class App extends Component<any, any> {
     }, 2000)
   }
 
+  updateUser = (user) => {
+    this.setState(
+      {
+        user: {
+          id: 1,
+          username: 'Vacho'
+        }
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <UserContext.Provider value={this.state.user}>
-          <TodoApp />
+          <UserUpdateContext.Provider value={this.updateUser}>
+            <TodoApp />
+          </UserUpdateContext.Provider>
         </UserContext.Provider>
 
       </div >
--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import AddTodo from "../AddTodo";
 import UserContext from '../../contexts/UserContext'
 
@@ -10,28 +10,45 @@ export interface IFilterProps {
   filterChange: (state: 'all' | 'active' | 'complete') => void
 }
 
-class Filter extends React.PureComponent<IFilterProps, any> {
-
-
-  render() {
-    const { incompleteCount, filterChange } = this.props;
-    const suffix = incompleteCount > 1 ? 's' : ''
-    return (
-      <div className="todo-filter">
-        <span> {incompleteCount} item{suffix} left </span>
-        <div className="todo-filter-buttons">
-          <button onClick={() => filterChange('all')}>All</button>
-          <button onClick={() => filterChange('active')}>Active</button>
-          <button onClick={() => filterChange('complete')}>Completed</button>
-        </div>
-        <UserContext.Consumer>
-          {(user) => {
-            return user ? "Authenticated: " + user.username : "Not Authenticated"
-          }}
-        </UserContext.Consumer>
+function Filter(props: IFilterProps) {
+  const user = useContext(UserContext);
+  const { incompleteCount, filterChange } = props;
+  const suffix = incompleteCount > 1 ? 's' : ''
+  return (
+    <div className="todo-filter">
+      <span> {incompleteCount} item{suffix} left </span>
+      <div className="todo-filter-buttons">
+        <button onClick={() => filterChange('all')}>All</button>
+        <button onClick={() => filterChange('active')}>Active</button>
+        <button onClick={() => filterChange('complete')}>Completed</button>
       </div>
-    );
-  }
+      {user ? "Authenticated: " + user.username : "Not Authenticated"}
+    </div>
+  );
 }
+
+// class Filter extends React.PureComponent<IFilterProps, any> {
+
+
+//   render() {
+//     const { incompleteCount, filterChange } = this.props;
+//     const suffix = incompleteCount > 1 ? 's' : ''
+//     return (
+//       <div className="todo-filter">
+//         <span> {incompleteCount} item{suffix} left </span>
+//         <div className="todo-filter-buttons">
+//           <button onClick={() => filterChange('all')}>All</button>
+//           <button onClick={() => filterChange('active')}>Active</button>
+//           <button onClick={() => filterChange('complete')}>Completed</button>
+//         </div>
+//         <UserContext.Consumer>
+//           {(user) => {
+//             return user ? "Authenticated: " + user.username : "Not Authenticated"
+//           }}
+//         </UserContext.Consumer>
+//       </div>
+//     );
+//   }
+// }
 
 export default Filter;
